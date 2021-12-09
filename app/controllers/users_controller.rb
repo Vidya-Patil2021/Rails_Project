9443f1d6bf04850eb700ae040a64805f3d381bc2
Vidya-Patil2021 <vidya@excelleresolutions.com>
2021-12-09 16:43:28 +0530

    Updated Codee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
@@ -12,19 +18,19 @@ class UsersController < ApplicationController
     end
   end
 
-    def edit
-      @user = User.find(params[:id])
-    end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-          flash[:notice]="Your account information was successfully updated"
-          redirect_to articles_path
-        else
-          render 'edit'
-        end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
+  end
 
   private
   def user_params
