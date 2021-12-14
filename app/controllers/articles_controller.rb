14a5484fff917d108c2f34d0c7a2aa3ed4ab2568
Vidya-Patil2021 <vidya@excelleresolutions.com>
2021-12-14 21:14:35 +0530

    Add admin to user access

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,12 +13,12 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+   if current_user != @article.user && !current_user.admin?
       flash[:alert] = "You can only edit or delete your own article"
       redirect_to @article
     end
   end
-  
+
 
   def new
     @article = Article.new
@@ -51,7 +51,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-
     @article.destroy
     redirect_to articles_path
   end
