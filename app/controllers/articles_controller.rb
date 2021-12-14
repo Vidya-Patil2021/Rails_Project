5a15f579288aefe75fdb0980b4b250288bbb946f
Vidya-Patil2021 <vidya@excelleresolutions.com>
2021-12-14 20:25:05 +0530

    Restrict actions for Articles Controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article,only:[:show,:edit,:update,:destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
 
   end
@@ -8,6 +11,14 @@ class ArticlesController < ApplicationController
   def index
    @articles = Article.paginate(page: params[:page], per_page: 2)
   end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
+  
 
   def new
     @article = Article.new
